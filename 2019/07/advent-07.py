b0e36973d477b513c8b34c6728d314262054625f
betoma <bethanyetoma@outlook.com>
2020-03-09 20:34:34 +0100

    small tweaks to intcode organization

--- 2019/07/advent-07.py
+++ 2019/07/advent-07.py
@@ -1,12 +1,45 @@
 from itertools import permutations
-import intcode
+from intcode import Intcode
+
+class Amplifiers():
+    def __init__(self,inputfile):
+        self.amplifiers = [
+            Intcode.read_program(inputfile),
+            Intcode.read_program(inputfile),
+            Intcode.read_program(inputfile),
+            Intcode.read_program(inputfile),
+            Intcode.read_program(inputfile)
+        ]
+    
+    def run(self,phase_settings=None,input_signal=0):
+        next_in = input_signal
+        for i, amp in enumerate(self.amplifiers):
+            if phase_settings is not None:
+                stack = [next_in, phase_settings[i]]
+            else:
+                stack = [next_in]
+            program = amp.run_program(stack)
+            next_in = next(program)
+        return next_in
+    
+    def recursive_run(self,phase_settings,input_signal=0):
+        lastE = self.run(phase_settings,input_signal)
+        while True:
+            #print(f"IN SIGNAL: {lastE}")
+            try:
+                lastE = self.run(input_signal=lastE)
+            except StopIteration:
+                break
+            #else:
+            #    print(f"OUT SIGNAL: lastE")
+        return lastE
 
 #---part one---#
 
 def part_one():
     finals = []
     for setting in permutations(range(5)):
-        a = intcode.Amplifiers("input.txt")
+        a = Amplifiers("input.txt")
         thrust = a.run(setting)
         finals.append(thrust)
 
@@ -19,7 +52,7 @@ def part_one():
 def part_two():
     finals = []
     for setting in permutations(range(5,10)):
-        b = intcode.Amplifiers("input.txt")
+        b = Amplifiers("input.txt")
         thrust = b.recursive_run(setting)
         finals.append(thrust)
     
--- 2019/11/intcode.py
+++ 2019/11/intcode.py
@@ -185,36 +185,3 @@ class Intcode:
                 if output is not None:
                     yield output
         return
-
-class Amplifiers():
-    def __init__(self,inputfile):
-        self.amplifiers = [
-            Intcode.read_program(inputfile),
-            Intcode.read_program(inputfile),
-            Intcode.read_program(inputfile),
-            Intcode.read_program(inputfile),
-            Intcode.read_program(inputfile)
-        ]
-    
-    def run(self,phase_settings=None,input_signal=0):
-        next_in = input_signal
-        for i, amp in enumerate(self.amplifiers):
-            if phase_settings is not None:
-                stack = [next_in, phase_settings[i]]
-            else:
-                stack = [next_in]
-            program = amp.run_program(stack)
-            next_in = next(program)
-        return next_in
-    
-    def recursive_run(self,phase_settings,input_signal=0):
-        lastE = self.run(phase_settings,input_signal)
-        while True:
-            #print(f"IN SIGNAL: {lastE}")
-            try:
-                lastE = self.run(input_signal=lastE)
-            except StopIteration:
-                break
-            #else:
-            #    print(f"OUT SIGNAL: lastE")
-        return lastE
