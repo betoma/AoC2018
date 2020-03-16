712947fc590ce76ccf21139c6b124742a0f23e6c
betoma <bethanyetoma@outlook.com>
2020-03-16 12:20:14 +0100

    finish day 14 of 2019

--- 2019/14/advent-14.py
+++ 2019/14/advent-14.py
@@ -37,21 +37,50 @@ class ReactionChain:
         recipe = self.reactions[ingredient]
         already_got = self.pantry.pop(ingredient, 0)
         need = n - already_got
-        batch = (ceil(need,recipe[0]),recipe)
-        #batch_nos  = [(ceil(n-already_got,x[0]), x) for x in recipes]
-        #if len(batch_nos) > 1:
-        #    batches = min(batch_nos)
-        #else:
-        times = batch[0]
-        increment = batch[1][0]
-        amounts = batch[1][1]
-        requirements = [((x*times),y) for (x,y) in amounts]
-        leftovers = (((times*increment) % n), ingredient)
-        self.pantry[leftovers[1]] = leftovers[0]
-        return requirements, leftovers
+        if need > 0:
+            remaining = 0
+            batch = (ceil(need,recipe[0]),recipe)
+            times = batch[0]
+            increment = batch[1][0]
+            amounts = batch[1][1]
+            requirements = {y: x*times for (x,y) in amounts}
+            leftovers = (((times*increment)- need), ingredient)
+        else:
+            leftovers = (0, ingredient)
+            remaining = -need
+            requirements = {'ORE': 0}
+        ingredients = [*requirements]
+        self.pantry[leftovers[1]] = remaining + leftovers[0]
+        return requirements, ingredients
 
-fu = ReactionChain("test.txt")
-print(fu.how_much('A',7))
-print(fu.how_much('E',1))
-print()
-print(fu.pantry)
+    def empty_pantry(self):
+        self.pantry = defaultdict(int)
+
+    def how_much_ore(self,ingredient,n:int=1):
+        amounts, components = self.how_much(ingredient,n)
+        #print(f"{n} {ingredient} requires {amounts}.")
+        #print(f"{self.pantry} leftover.")
+        if components.count('ORE') == len(components) and components:
+            return amounts['ORE']
+        else:
+            sum_list = [self.how_much_ore(x,amounts[x]) for x in components]
+            return sum(sum_list)
+
+    def how_much_fuel(self,n:int=100):
+        test_amount = n // self.how_much_ore('FUEL')
+        tried = set()
+        while True:
+            self.empty_pantry()
+            actual_amount = self.how_much_ore('FUEL',int(test_amount))
+            if actual_amount not in tried:
+                tried.add(actual_amount)
+            elif actual_amount <= n:
+                return int(test_amount)
+            multiple = n / actual_amount
+            test_amount *= multiple
+
+
+
+fu = ReactionChain("input.txt")
+#print(fu.how_much_ore('FUEL'))
+print(fu.how_much_fuel(1000000000000))
