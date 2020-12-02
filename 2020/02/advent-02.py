6e9233802e16514ec8d90b6d3030075452c0b599
Bethany E. Toma <bethany@thetalake.com>
2020-12-02 12:31:05 +0100

    add day 2 of 2020

---
+++ 2020/02/advent-02.py
@@ -0,0 +1,38 @@
+import re
+
+with open("input.txt", "r") as f:
+    content = [re.split(r"-|:\s| ", line) for line in f.readlines()]
+
+passwords = [(int(z[0]), int(z[1]), z[2], z[3].strip()) for z in content]
+
+# part one
+
+
+def password_check(low_n: int, high_n: int, letter: str, string: str):
+    if (c := string.count(letter)) <= high_n:
+        if c >= low_n:
+            return True
+    return False
+
+
+password_good = [password_check(*p) for p in passwords]
+
+print(password_good.count(True))
+
+
+# part two
+
+
+def new_password_check(i_1: int, i_2: int, letter: str, string: str):
+    if string[i_1 - 1] == letter:
+        if string[i_2 - 1] == letter:
+            return False
+        return True
+    elif string[i_2 - 1] == letter:
+        return True
+    return False
+
+
+password_better = [new_password_check(*p) for p in passwords]
+
+print(password_better.count(True))
